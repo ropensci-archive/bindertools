ae506e612d96ef1b6cf9381d611367e4febe5d9d
Saras Windecker <swindecker@9770L-134432-M.local>
2018-11-23 13:10:05 +1100

    create install.R file

---
+++ binder_installR.R
@@ -0,0 +1,102 @@
+library(purrr)
+
+
+binder_installR <- function (directory = '.') {
+  
+  R_files <- dir(directory, 
+                 full.names = TRUE,
+                 pattern = "\\.(rmd|r)$" , 
+                 recursive = TRUE, 
+                 ignore.case = TRUE)
+ #  if (length(R_files == 0)) stop('No R files found.')
+  
+  lib_list <-
+    purrr::map(R_files, readLines) %>%
+    purrr::map(find_doc_libs) %>%
+    unlist() %>%
+    unique()
+  
+  CRAN_packages <- purrr::map_dfr(lib_list, CRAN_package) %>%
+    Filter(Negate(is.null), .) 
+  
+  github_packages <- purrr::map(lib_list, github_package) %>%
+    Filter(Negate(is.null), .) %>%
+    as.character()
+  
+  x_CRAN <- paste0("purrr::walk2(.x = ", 
+                   deparse(CRAN_packages$package), 
+                   ", .y = ", 
+                   deparse(CRAN_packages$version), 
+                   ", ~devtools::install_version(package = .x, version = .y))")
+  
+  x_github <- paste0("devtools::install_github(c(", 
+                     paste0("\"", github_packages, "\"", collapse = ", "), "))")
+  
+  # add to install.R file  
+  writeLines(paste0(x_CRAN, '\n', x_github), 'install.R')
+
+}
+
+
+CRAN_package <- function (package_name) {
+  
+  pd <- packageDescription(package_name)
+
+  if (is.null(pd$Repository)) {
+    return (NULL)
+  }
+  
+  if (!is.null(pd$Repository)) {
+    return(list(package = package_name, 
+                version = pd$Version))
+  }
+
+}
+
+github_package <- function (package_name) {
+  
+  pd <- packageDescription(package_name)
+  
+  if (is.null(pd$Repository)) {
+    return(paste0(pd$RemoteUsername, '/', pd$GithubRepo, '@', pd$RemoteSha))
+  }
+  
+  if (!is.null(pd$Repository)) {
+    return(NULL)
+  }
+  
+}
+
+
+
+# from milesmcbain/deplearning
+find_doc_libs <- function (doc) {
+  
+  # filter comments
+  comments <- "^\\s*#"
+  comment_lines <- grepl(pattern = comments, x = doc)
+  doc <- doc[!comment_lines]
+  
+  # find dependencies
+  patterns <- list(
+    library = "(?<=library\\()\"*[a-zA-Z0-9.]+\"*(?=\\))",
+    require   = "(?<=require\\()\"*[a-zA-Z0-9.]+\"*(?=\\))",
+    p_load = "(?<=p_load\\()\"*[a-zA-Z0-9.]+\"*[\\s*\\,\\s*\"*[a-zA-Z0-9.]+\"*]*(?=\\))",
+    `::` = "[a-zA-Z0-9.]+(?=::)"
+  )
+  
+  match_pos <- purrr::map(patterns, ~regexec(., doc, perl = TRUE))
+  lib_matches <- purrr::map(match_pos, ~regmatches(doc, .)) %>%
+    lapply(unlist)
+  
+  # post processing cleanup of matches
+  final_matches <-
+    lib_matches %>%
+    purrr::map(~gsub(x = ., pattern = "\"", replacement ="")) %>%
+    purrr::map(~strsplit(x = . , split = ",\\s*" )) %>%
+    unlist() %>%
+    unique()
+  
+  final_matches
+}
+
